d544f22e2d4061bd6cc9dbadd94d89a6b330fdc3
Sergio <https://plaroser@bitbucket.org/plaroser/pruebaandroid.git>
2017-12-12 11:02:08 +0100

    Clase juego refactorizada

--- juego de la vida/juego_de_la_vida.py
+++ juego de la vida/juego_de_la_vida.py
@@ -2,23 +2,6 @@ import ControladorTablero
 from tkinter import *
 if __name__ == '__main__':
     c = ControladorTablero.ControladorTablero(10,10)
-    #c.
-    #c.crearTablero(10,10)
-    #c.leerTablero()
-    #c.leerTablero()
-    #c.siguienteEstado()
-    '''ControladorTablero.ControladorTablero.siguienteEstado()
-    ControladorTablero.ControladorTablero.siguienteEstado()
-
-    ControladorTablero.ControladorTablero.siguienteEstado()
-    ControladorTablero.ControladorTablero.siguienteEstado()'''
-    
+   
     mainloop()
-    '''
-    t.activar(1,1)
-    t.activar(2,1)
-    t.activar(3,1)
     
-    t.evolucion()
-    '''
-    #print(str(t))
